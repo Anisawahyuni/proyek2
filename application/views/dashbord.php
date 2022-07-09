dfd08473e15cce287794a0df69480adbcc7b727d
Anisawahyuni <annisaoye2@gmail.com>
2022-07-09 16:02:52 +0700

    proyekUMKM

---
+++ application/views/dashbord.php
@@ -0,0 +1,63 @@
+<section class="section" id="chefs">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4 offset-lg-4 text-center">
+                    <div class="section-heading">
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="chef-item">
+                        <div class="thumb">
+                            <div class="overlay"></div>
+                            <img src="<?php echo base_url('assets/images/WhatsApp Image 2022-07-08 at 23.51.37.jpeg')?>" alt="Chef #1"height="300" width="200">
+                        </div>
+                        <div class="down-content">
+                            <h4>Fathiya Anandita</h4>
+                            <span>0110121295</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-10">
+                    <div class="chef-item">
+                        <div class="thumb">
+                            <div class="overlay"></div>
+                            <ul class="social-icons">
+                            </ul>
+                            <img src="<?php echo base_url('assets/images/WhatsApp Image 2022-07-08 at 23.50.56.jpeg')?>" alt="Chef #2"height="300" width="200">
+                        </div>
+                        <div class="down-content">
+                            <h4>Shofa Farhatul Muslimah</h4>
+                            <span>011121309</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="chef-item">
+                        <div class="thumb">
+                            <div class="overlay"></div>
+                            <img src="<?php echo base_url('assets/images/IMG-20220626-WA0026.jpg')?>" alt="Chef #3"height="300" width="200">
+                        </div>
+                        <div class="down-content">
+                            <h4>Anisa Wahyuni</h4>
+                            <span>0110121300</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6">
+                    <div class="chef-item">
+                        <div class="thumb">
+                            <div class="overlay"></div>
+                            <ul class="social-icons">
+                            </ul>
+                            <img src="<?php echo base_url('assets/images/WhatsApp Image 2022-07-09 at 09.43.23.jpeg')?>" alt="Chef #2"height="300" width="200">
+                        </div>
+                        <div class="down-content">
+                            <h4>Sunarsih</h4>
+                            <span>011121304</span>
+                        </div>
+                    </div>
+                </div> 
+    </section>
